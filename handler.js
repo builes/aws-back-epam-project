cd13e835e852b9c5e590c66c5bcb0fad4839e624
Mauricio Builes <rmbuilesm@unal.edu.co>
2023-10-20 03:19:18 -0500

    added getProductsById

--- handler.js
+++ handler.js
@@ -1,79 +1,8 @@
 "use strict";
 
-module.exports.getProductList = async (event) => {
-  const products = [
-    {
-      id: 1,
-      Product: "Smartphone",
-      Name: "iPhone 13 Pro",
-      Manufacturer: "Apple Inc.",
-      Price: "$999",
-    },
-    {
-      id: 2,
-      Product: "Laptop",
-      Name: "MacBook Air",
-      Manufacturer: "Apple Inc.",
-      Price: "$999",
-    },
-    {
-      id: 3,
-      Product: "Smartwatch",
-      Name: "Apple Watch Series 7",
-      Manufacturer: "Apple Inc.",
-      Price: "$399",
-    },
-    {
-      id: 4,
-      Product: "Gaming Console",
-      Name: "PlayStation 5",
-      Manufacturer: "Sony Interactive Entertainment",
-      Price: "$499",
-    },
-    {
-      id: 5,
-      Product: "Tablet",
-      Name: "iPad Pro",
-      Manufacturer: "Apple Inc.",
-      Price: "$799",
-    },
-    {
-      id: 6,
-      Product: "Smart Speaker",
-      Name: "Amazon Echo Dot (4th Gen)",
-      Manufacturer: "Amazon",
-      Price: "$49.99",
-    },
-    {
-      id: 7,
-      Product: "Wireless Headphones",
-      Name: "Sony WH-1000XM4",
-      Manufacturer: "Sony",
-      Price: "$349.99",
-    },
-    {
-      id: 8,
-      Product: "Digital Camera",
-      Name: "Canon EOS 90D",
-      Manufacturer: "Canon",
-      Price: "$1199",
-    },
-    {
-      id: 9,
-      Product: "4K TV",
-      Name: "LG OLED C1 Series",
-      Manufacturer: "LG",
-      Price: "$1499",
-    },
-    {
-      id: 10,
-      Product: "Router",
-      Name: "NETGEAR Nighthawk AX12",
-      Manufacturer: "NETGEAR",
-      Price: "$399",
-    },
-  ];
+const products = require("./products");
 
+module.exports.getProductList = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -87,4 +16,34 @@ module.exports.getProductList = async (event) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getProductsById = async (event) => {
+  const { id } = event.pathParameters;
+
+  const product = products.find((product) => product.id === Number(id));
+
+  if (!product) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify(
+        {
+          error: "Product not found",
+        },
+        null,
+        2
+      ),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        product,
+      },
+      null,
+      2
+    ),
+  };
 };
---
+++ products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    Product: "Smartphone",
+    Name: "iPhone 13 Pro",
+    Manufacturer: "Apple Inc.",
+    Price: "$999",
+  },
+  {
+    id: 2,
+    Product: "Laptop",
+    Name: "MacBook Air",
+    Manufacturer: "Apple Inc.",
+    Price: "$999",
+  },
+  {
+    id: 3,
+    Product: "Smartwatch",
+    Name: "Apple Watch Series 7",
+    Manufacturer: "Apple Inc.",
+    Price: "$399",
+  },
+  {
+    id: 4,
+    Product: "Gaming Console",
+    Name: "PlayStation 5",
+    Manufacturer: "Sony Interactive Entertainment",
+    Price: "$499",
+  },
+  {
+    id: 5,
+    Product: "Tablet",
+    Name: "iPad Pro",
+    Manufacturer: "Apple Inc.",
+    Price: "$799",
+  },
+  {
+    id: 6,
+    Product: "Smart Speaker",
+    Name: "Amazon Echo Dot (4th Gen)",
+    Manufacturer: "Amazon",
+    Price: "$49.99",
+  },
+  {
+    id: 7,
+    Product: "Wireless Headphones",
+    Name: "Sony WH-1000XM4",
+    Manufacturer: "Sony",
+    Price: "$349.99",
+  },
+  {
+    id: 8,
+    Product: "Digital Camera",
+    Name: "Canon EOS 90D",
+    Manufacturer: "Canon",
+    Price: "$1199",
+  },
+  {
+    id: 9,
+    Product: "4K TV",
+    Name: "LG OLED C1 Series",
+    Manufacturer: "LG",
+    Price: "$1499",
+  },
+  {
+    id: 10,
+    Product: "Router",
+    Name: "NETGEAR Nighthawk AX12",
+    Manufacturer: "NETGEAR",
+    Price: "$399",
+  },
+];
+
+module.exports = products;
